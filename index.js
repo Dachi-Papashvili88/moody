74255b31e398a6ec9708bb1144a842f407dc0835
dachi Papashvili <dachi123.dp@gmail.com>
2023-11-29 00:56:56 +0400

    firebase authentication

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-/* === Imports === */
-import { initializeApp } from "firebase/app";
 /* === Firebase Setup === */
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -17,6 +17,8 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
 /* === UI === */
 
 /* == UI - Elements == */
