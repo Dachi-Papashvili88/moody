3171fd9089f0557a20943ba4d4a485412d25d3c0
dachi Papashvili <dachi123.dp@gmail.com>
2024-01-01 23:20:00 +0400

    fetching by day

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ import {
   serverTimestamp,
   onSnapshot,
   query,
-  where,
+  where, orderBy 
 } from "firebase/firestore";
 
 const firebaseConfig = {
@@ -36,148 +36,166 @@ const db = getFirestore(app);
 
 /* == UI - Elements == */
 
-const viewLoggedOut = document.getElementById("logged-out-view");
-const viewLoggedIn = document.getElementById("logged-in-view");
+const viewLoggedOut = document.getElementById("logged-out-view")
+const viewLoggedIn = document.getElementById("logged-in-view")
 
-const signInWithGoogleButtonEl = document.getElementById(
-  "sign-in-with-google-btn"
-);
+const signInWithGoogleButtonEl = document.getElementById("sign-in-with-google-btn")
 
-const emailInputEl = document.getElementById("email-input");
-const passwordInputEl = document.getElementById("password-input");
+const emailInputEl = document.getElementById("email-input")
+const passwordInputEl = document.getElementById("password-input")
 
-const signInButtonEl = document.getElementById("sign-in-btn");
-const createAccountButtonEl = document.getElementById("create-account-btn");
+const signInButtonEl = document.getElementById("sign-in-btn")
+const createAccountButtonEl = document.getElementById("create-account-btn")
 
-const signOutButtonEl = document.getElementById("sign-out-btn");
+const signOutButtonEl = document.getElementById("sign-out-btn")
 
-const userProfilePictureEl = document.getElementById("user-profile-picture");
-const userGreetingEl = document.getElementById("user-greeting");
+const userProfilePictureEl = document.getElementById("user-profile-picture")
+const userGreetingEl = document.getElementById("user-greeting")
 
-const moodEmojiEls = document.getElementsByClassName("mood-emoji-btn");
-const textareaEl = document.getElementById("post-input");
-const postButtonEl = document.getElementById("post-btn");
+const moodEmojiEls = document.getElementsByClassName("mood-emoji-btn")
+const textareaEl = document.getElementById("post-input")
+const postButtonEl = document.getElementById("post-btn")
 
-const postsEl = document.getElementById("posts");
+const allFilterButtonEl = document.getElementById("all-filter-btn")
+
+const filterButtonEls = document.getElementsByClassName("filter-btn")
+
+const postsEl = document.getElementById("posts")
 
 /* == UI - Event Listeners == */
 
-signInWithGoogleButtonEl.addEventListener("click", authSignInWithGoogle);
+signInWithGoogleButtonEl.addEventListener("click", authSignInWithGoogle)
 
-signInButtonEl.addEventListener("click", authSignInWithEmail);
-createAccountButtonEl.addEventListener("click", authCreateAccountWithEmail);
+signInButtonEl.addEventListener("click", authSignInWithEmail)
+createAccountButtonEl.addEventListener("click", authCreateAccountWithEmail)
 
-signOutButtonEl.addEventListener("click", authSignOut);
+signOutButtonEl.addEventListener("click", authSignOut)
 
 for (let moodEmojiEl of moodEmojiEls) {
-  moodEmojiEl.addEventListener("click", selectMood);
+    moodEmojiEl.addEventListener("click", selectMood)
 }
 
-postButtonEl.addEventListener("click", postButtonPressed);
+for (let filterButtonEl of filterButtonEls) {
+    filterButtonEl.addEventListener("click", selectFilter)
+}
+
+postButtonEl.addEventListener("click", postButtonPressed)
 
 /* === State === */
 
-let moodState = 0;
+let moodState = 0
 
 /* === Global Constants === */
 
-const collectionName = "posts";
+const collectionName = "posts"
 
 /* === Main Code === */
 
 onAuthStateChanged(auth, (user) => {
-  if (user) {
-    showLoggedInView();
-    showProfilePicture(userProfilePictureEl, user);
-    showUserGreeting(userGreetingEl, user);
-    fetchInRealtimeAndRenderPostsFromDB(user);
-  } else {
-    showLoggedOutView();
-  }
-});
+    if (user) {
+        showLoggedInView()
+        showProfilePicture(userProfilePictureEl, user)
+        showUserGreeting(userGreetingEl, user)
+        //fetchInRealtimeAndRenderPostsFromDB(user)
+    } else {
+        showLoggedOutView()
+    }
+})
 
 /* === Functions === */
 
 /* = Functions - Firebase - Authentication = */
 
 function authSignInWithGoogle() {
-  signInWithPopup(auth, provider)
-    .then((result) => {
-      console.log("Signed in with Google");
-    })
-    .catch((error) => {
-      console.error(error.message);
-    });
+    signInWithPopup(auth, provider)
+        .then((result) => {
+            console.log("Signed in with Google")
+        }).catch((error) => {
+            console.error(error.message)
+        })
 }
 
 function authSignInWithEmail() {
-  const email = emailInputEl.value;
-  const password = passwordInputEl.value;
-
-  signInWithEmailAndPassword(auth, email, password)
-    .then((userCredential) => {
-      clearAuthFields();
-    })
-    .catch((error) => {
-      console.error(error.message);
-    });
+    const email = emailInputEl.value
+    const password = passwordInputEl.value
+    
+    signInWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+            clearAuthFields()
+        })
+        .catch((error) => {
+            console.error(error.message)
+        })
 }
 
 function authCreateAccountWithEmail() {
-  const email = emailInputEl.value;
-  const password = passwordInputEl.value;
+    const email = emailInputEl.value
+    const password = passwordInputEl.value
 
-  createUserWithEmailAndPassword(auth, email, password)
-    .then((userCredential) => {
-      clearAuthFields();
-    })
-    .catch((error) => {
-      console.error(error.message);
-    });
+    createUserWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+            clearAuthFields()
+        })
+        .catch((error) => {
+            console.error(error.message) 
+        })
 }
 
 function authSignOut() {
-  signOut(auth)
-    .then(() => {})
-    .catch((error) => {
-      console.error(error.message);
-    });
+    signOut(auth)
+        .then(() => {
+        }).catch((error) => {
+            console.error(error.message)
+        })
 }
 
 /* = Functions - Firebase - Cloud Firestore = */
 
 async function addPostToDB(postBody, user) {
-  try {
-    const docRef = await addDoc(collection(db, collectionName), {
-      body: postBody,
-      uid: user.uid,
-      createdAt: serverTimestamp(),
-      mood: moodState,
-    });
-    console.log("Document written with ID: ", docRef.id);
-  } catch (error) {
-    console.error(error.message);
-  }
+    try {
+        const docRef = await addDoc(collection(db, collectionName), {
+            body: postBody,
+            uid: user.uid,
+            createdAt: serverTimestamp(),
+            mood: moodState
+        })
+        console.log("Document written with ID: ", docRef.id)
+    } catch (error) {
+        console.error(error.message)
+    }
 }
 
-function fetchInRealtimeAndRenderPostsFromDB(user) {
-  const postRef = collection(db, collectionName);
-
-const q = query(postRef, where("uid", "==", user.uid));
-
-  onSnapshot(q, (querySnapshot) => {
-    clearAll(postsEl)
-    
-    querySnapshot.forEach((doc) => {
-        renderPost(postsEl, doc.data())
+function fetchInRealtimeAndRenderPostsFromDB(query, user) {
+    onSnapshot(query, (querySnapshot) => {
+        clearAll(postsEl)
+        
+        querySnapshot.forEach((doc) => {
+            renderPost(postsEl, doc.data())
+        })
     })
-})
+}
+
+function fetchTodayPosts(user) {
+    const startOfDay = new Date()
+    startOfDay.setHours(0, 0, 0, 0)
+    
+    const endOfDay = new Date()
+    endOfDay.setHours(23, 59, 59, 999)
+    
+    const postsRef = collection(db, collectionName)
+    
+    const q = query(postsRef, where("uid", "==", user.uid),
+                              where("createdAt", ">=", startOfDay),
+                              where("createdAt", "<=", endOfDay),
+                              orderBy("createdAt", "desc"))
+                              
+    fetchInRealtimeAndRenderPostsFromDB(q, user)                  
 }
 
 /* == Functions - UI Functions == */
 
 function renderPost(postsEl, postData) {
-  postsEl.innerHTML += `
+    postsEl.innerHTML += `
         <div class="post">
             <div class="header">
                 <h3>${displayDate(postData.createdAt)}</h3>
@@ -187,148 +205,159 @@ function renderPost(postsEl, postData) {
                 ${replaceNewlinesWithBrTags(postData.body)}
             </p>
         </div>
-    `;
+    `
 }
 
 function replaceNewlinesWithBrTags(inputString) {
-  return inputString.replace(/\n/g, "<br>");
+    return inputString.replace(/\n/g, "<br>")
 }
 
 function postButtonPressed() {
-  const postBody = textareaEl.value;
-  const user = auth.currentUser;
-
-  if (postBody && moodState) {
-    addPostToDB(postBody, user);
-    clearInputField(textareaEl);
-    resetAllMoodElements(moodEmojiEls);
-  }
+    const postBody = textareaEl.value
+    const user = auth.currentUser
+    
+    if (postBody && moodState) {
+        addPostToDB(postBody, user)
+        clearInputField(textareaEl)
+        resetAllMoodElements(moodEmojiEls)
+    }
 }
 
 function clearAll(element) {
-  element.innerHTML = "";
+    element.innerHTML = ""
 }
 
 function showLoggedOutView() {
-  hideView(viewLoggedIn);
-  showView(viewLoggedOut);
+    hideView(viewLoggedIn)
+    showView(viewLoggedOut)
 }
 
 function showLoggedInView() {
-  hideView(viewLoggedOut);
-  showView(viewLoggedIn);
+    hideView(viewLoggedOut)
+    showView(viewLoggedIn)
 }
 
 function showView(view) {
-  view.style.display = "flex";
+    view.style.display = "flex" 
 }
 
 function hideView(view) {
-  view.style.display = "none";
+    view.style.display = "none"
 }
 
 function clearInputField(field) {
-  field.value = "";
+	field.value = ""
 }
 
 function clearAuthFields() {
-  clearInputField(emailInputEl);
-  clearInputField(passwordInputEl);
+	clearInputField(emailInputEl)
+	clearInputField(passwordInputEl)
 }
 
 function showProfilePicture(imgElement, user) {
-  const photoURL = user.photoURL;
-
-  if (photoURL) {
-    imgElement.src = photoURL;
-  } else {
-    imgElement.src = "assets/images/default-profile-picture.jpeg";
-  }
+    const photoURL = user.photoURL
+    
+    if (photoURL) {
+        imgElement.src = photoURL
+    } else {
+        imgElement.src = "assets/images/default-profile-picture.jpeg"
+    }
 }
 
 function showUserGreeting(element, user) {
-  const displayName = user.displayName;
-
-  if (displayName) {
-    const userFirstName = displayName.split(" ")[0];
-
-    element.textContent = `Hey ${userFirstName}, how are you?`;
-  } else {
-    element.textContent = `Hey friend, how are you?`;
-  }
+    const displayName = user.displayName
+    
+    if (displayName) {
+        const userFirstName = displayName.split(" ")[0]
+        
+        element.textContent = `Hey ${userFirstName}, how are you?`
+    } else {
+        element.textContent = `Hey friend, how are you?`
+    }
 }
 
 function displayDate(firebaseDate) {
-  if (!firebaseDate) {
-    return "Date processing";
-  }
-  const date = firebaseDate.toDate();
+    if (!firebaseDate) {
+        return "Date processing"
+    }
+    
+    const date = firebaseDate.toDate()
+    
+    const day = date.getDate()
+    const year = date.getFullYear()
+    
+    const monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+    const month = monthNames[date.getMonth()]
 
-  const day = date.getDate();
-  const year = date.getFullYear();
+    let hours = date.getHours()
+    let minutes = date.getMinutes()
+    hours = hours < 10 ? "0" + hours : hours
+    minutes = minutes < 10 ? "0" + minutes : minutes
 
-  const monthNames = [
-    "Jan",
-    "Feb",
-    "Mar",
-    "Apr",
-    "May",
-    "Jun",
-    "Jul",
-    "Aug",
-    "Sep",
-    "Oct",
-    "Nov",
-    "Dec",
-  ];
-  const month = monthNames[date.getMonth()];
-
-  let hours = date.getHours();
-  let minutes = date.getMinutes();
-  hours = hours < 10 ? "0" + hours : hours;
-  minutes = minutes < 10 ? "0" + minutes : minutes;
-
-  return `${day} ${month} ${year} - ${hours}:${minutes}`;
+    return `${day} ${month} ${year} - ${hours}:${minutes}`
 }
 
 /* = Functions - UI Functions - Mood = */
 
 function selectMood(event) {
-  const selectedMoodEmojiElementId = event.currentTarget.id;
-
-  changeMoodsStyleAfterSelection(selectedMoodEmojiElementId, moodEmojiEls);
-
-  const chosenMoodValue = returnMoodValueFromElementId(
-    selectedMoodEmojiElementId
-  );
-
-  moodState = chosenMoodValue;
+    const selectedMoodEmojiElementId = event.currentTarget.id
+    
+    changeMoodsStyleAfterSelection(selectedMoodEmojiElementId, moodEmojiEls)
+    
+    const chosenMoodValue = returnMoodValueFromElementId(selectedMoodEmojiElementId)
+    
+    moodState = chosenMoodValue
 }
 
-function changeMoodsStyleAfterSelection(
-  selectedMoodElementId,
-  allMoodElements
-) {
-  for (let moodEmojiEl of moodEmojiEls) {
-    if (selectedMoodElementId === moodEmojiEl.id) {
-      moodEmojiEl.classList.remove("unselected-emoji");
-      moodEmojiEl.classList.add("selected-emoji");
-    } else {
-      moodEmojiEl.classList.remove("selected-emoji");
-      moodEmojiEl.classList.add("unselected-emoji");
+function changeMoodsStyleAfterSelection(selectedMoodElementId, allMoodElements) {
+    for (let moodEmojiEl of moodEmojiEls) {
+        if (selectedMoodElementId === moodEmojiEl.id) {
+            moodEmojiEl.classList.remove("unselected-emoji")          
+            moodEmojiEl.classList.add("selected-emoji")
+        } else {
+            moodEmojiEl.classList.remove("selected-emoji")
+            moodEmojiEl.classList.add("unselected-emoji")
+        }
     }
-  }
 }
 
 function resetAllMoodElements(allMoodElements) {
-  for (let moodEmojiEl of allMoodElements) {
-    moodEmojiEl.classList.remove("selected-emoji");
-    moodEmojiEl.classList.remove("unselected-emoji");
-  }
-
-  moodState = 0;
+    for (let moodEmojiEl of allMoodElements) {
+        moodEmojiEl.classList.remove("selected-emoji")
+        moodEmojiEl.classList.remove("unselected-emoji")
+    }
+    
+    moodState = 0
 }
 
 function returnMoodValueFromElementId(elementId) {
-  return Number(elementId.slice(5));
+    return Number(elementId.slice(5))
+}
+
+/* == Functions - UI Functions - Date Filters == */
+
+function resetAllFilterButtons(allFilterButtons) {
+    for (let filterButtonEl of allFilterButtons) {
+        filterButtonEl.classList.remove("selected-filter")
+    }
+}
+
+function updateFilterButtonStyle(element) {
+    element.classList.add("selected-filter")
+}
+
+function selectFilter(event) {
+    const user = auth.currentUser
+    
+    const selectedFilterElementId = event.target.id
+    
+    const selectedFilterPeriod = selectedFilterElementId.split("-")[0]
+    
+    const selectedFilterElement = document.getElementById(selectedFilterElementId)
+    
+    resetAllFilterButtons(filterButtonEls)
+    
+    updateFilterButtonStyle(selectedFilterElement)
+    
+    fetchTodayPosts(user)
 }
